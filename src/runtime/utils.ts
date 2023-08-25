d511a30e7a8107bff893d95cfec18f847a4e06f0
Matthew Messinger <mattmess1221@gmail.com>
2023-08-25 01:14:37 -0400

    fix: allow pathParams to be a object ref

--- src/runtime/utils.ts
+++ src/runtime/utils.ts
@@ -41,12 +41,12 @@ export async function deserializeMaybeEncodedBody(value: ApiFetchOptions['body']
   return value
 }
 
-export function resolvePath(path: string, params?: Record<string, string>) {
+export function resolvePath(path: string, params?: Record<string, MaybeRefOrGetter<unknown>>) {
   // To simplify typings, OpenAPI path parameters can be expanded here
   if (params) {
     return Object.entries(params).reduce(
       (path, [name, value]) =>
-        path.replace(`{${name}}`, encodeURIComponent(value)),
+        path.replace(`{${name}}`, encodeURIComponent(String(toValue(value)))),
       path,
     )
   }
