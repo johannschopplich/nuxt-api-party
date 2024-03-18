johannschopplich/nuxt-api-party

commit 5401fdd6664753a6b3d7b35cd02a17d67b1a806e

refactor: simplify types generation

src/module.ts
@@ -143,12 +143,12 @@ export default defineNuxtModule<ModuleOptions>({
     // Nuxt will resolve the imports relative to the `srcDir`, so we can't use
     // `#nuxt-api-party` with `declare module` pattern here
     addImportsSources({
-      from: resolve(nuxt.options.buildDir, `module/${moduleName}-imports.mjs`),
+      from: resolve(nuxt.options.buildDir, `module/${moduleName}.mjs`),
       imports: endpointKeys.flatMap(i => [getRawComposableName(i), getDataComposableName(i)]),
     })
 
     // Add `#nuxt-api-party` module alias for generated composables
-    nuxt.options.alias[`#${moduleName}`] = resolve(nuxt.options.buildDir, `module/${moduleName}-imports.mjs`)
+    nuxt.options.alias[`#${moduleName}`] = resolve(nuxt.options.buildDir, `module/${moduleName}.mjs`)
 
     const relativeTo = (path: string) => relative(
       resolve(nuxt.options.rootDir, nuxt.options.buildDir, 'module'),
@@ -157,7 +157,7 @@ export default defineNuxtModule<ModuleOptions>({
 
     // Add module template for generated composables
     addTemplate({
-      filename: `module/${moduleName}-imports.mjs`,
+      filename: `module/${moduleName}.mjs`,
       getContents() {
         return `
 import { _$api } from '${resolve('runtime/composables/$api')}'
@@ -181,50 +181,39 @@ export const ${getDataComposableName(i)} = (...args) => _useApiData('${i}', ...a
       schemaEndpointIds.length = 0
     }
 
-    // Use generated types for generated composables for
-    // (1) Nuxt auto-imports
-    // (2) global import from `#nuxt-api-party`
-    const typesSchema = `
+    // Add types for Nuxt auto-imports and the `#nuxt-api-party` module alias
+    addTemplate({
+      filename: `module/${moduleName}.d.ts`,
+      getContents() {
+        return `
 // Generated by ${moduleName}
-import type { $AnyApi, $OpenApi } from '${relativeTo('runtime/composables/$api')}'
-import type { UseAnyApiData, UseOpenApiData } from '${relativeTo('runtime/composables/useApiData')}'
+import type { $Api, $AnyApi, $OpenApi, AnyApiFetchOptions } from '${relativeTo('runtime/composables/$api')}'
+import type { UseApiData, UseAnyApiData, UseOpenApiData, UseAnyApiDataOptions } from '${relativeTo('runtime/composables/useApiData')}'
 
 ${schemaEndpointIds.map(i => `import type { paths as ${pascalCase(i)}Paths } from '#${moduleName}/${i}'`).join('')}
+
+export type { $Api, $AnyApi, $OpenApi, AnyApiFetchOptions, UseApiData, UseAnyApiData, UseOpenApiData, UseAnyApiDataOptions }
 
 ${endpointKeys.map(i => `
 export declare const ${getRawComposableName(i)}: ${schemaEndpointIds.includes(i) ? `$OpenApi<${pascalCase(i)}Paths>` : '$AnyApi'}
 export declare const ${getDataComposableName(i)}: ${schemaEndpointIds.includes(i) ? `UseOpenApiData<${pascalCase(i)}Paths>` : 'UseAnyApiData'}
 `.trimStart()).join('').trimEnd()}
 `.trimStart()
-
-    // Add types for Nuxt auto-imports
-    addTemplate({
-      filename: `module/${moduleName}-imports.d.ts`,
-      getContents() {
-        return typesSchema
       },
     })
 
-    // Add types for global aliases
-    addTemplate({
-      filename: `module/${moduleName}.d.ts`,
-      async getContents() {
-        return `
-declare module '#${moduleName}' {
-${typesSchema.replace(/^/gm, '  ').trimEnd()}
-}
+    // Add type references for endpoints with OpenAPI schemas
+    if (schemaEndpointIds.length) {
+      addTemplate({
+        filename: `module/${moduleName}-schema.d.ts`,
+        async getContents() {
+          return await generateTypes(schemaEndpoints, resolvedOptions.openAPITS)
+        },
+      })
 
-${schemaEndpointIds.length
-  ? await generateTypes(schemaEndpoints, resolvedOptions.openAPITS)
-  : ''}
-`.trimStart()
-      },
-    })
-
-    // Add type references to TypeScript config
-    nuxt.hook('prepare:types', (options) => {
-      options.references.push({ path: resolve(nuxt.options.buildDir, `module/${moduleName}.d.ts`) })
-      options.references.push({ path: resolve(nuxt.options.buildDir, `module/${moduleName}-imports.d.ts`) })
-    })
+      nuxt.hook('prepare:types', (options) => {
+        options.references.push({ path: resolve(nuxt.options.buildDir, `module/${moduleName}-schema.d.ts`) })
+      })
+    }
   },
 })

src/kit.ts
@@ -1,0 +1,30 @@
+import { relative } from 'pathe'
+import { addTemplate, createResolver, useNuxt } from '@nuxt/kit'
+
+export function extendTypes(
+  module: string,
+  template: (options: { typesPath: string }) => string | Promise<string>,
+) {
+  const nuxt = useNuxt()
+  const { resolve } = createResolver(import.meta.url)
+
+  addTemplate({
+    filename: `module/${module}.d.ts`,
+    getContents: async () => {
+      const typesPath = relative(
+        resolve(nuxt.options.rootDir, nuxt.options.buildDir, 'module'),
+        resolve('runtime/types'),
+      )
+      const s = await template({ typesPath })
+      return `
+// Generated by ${module}
+${s}`.trimStart()
+    },
+  })
+
+  nuxt.hooks.hook('prepare:types', ({ references }) => {
+    references.push({
+      path: resolve(nuxt.options.buildDir, `module/${module}.d.ts`),
+    })
+  })
+}

commit 64befbb29c296e17e58c6dbcbd59a39111bf1439

fix: remove extension from import sources
This fixes an issue (introduced in 5401fdd6664753a6b3d7b35cd02a17d67b1a806e), which prevents typescript from seeing the module when the .mjs file is not generated, but .d.ts is.

src/module.ts
@@ -211,7 +211,7 @@ export const ${getRawComposableName(i)} = (...args) => _$api('${i}', ...args)
 
     // Add Nuxt auto-imports for generated composables
     addImportsSources({
-      from: resolve(nuxt.options.buildDir, `module/${moduleName}.mjs`),
+      from: resolve(nuxt.options.buildDir, `module/${moduleName}`),
       imports: endpointKeys.flatMap(i => [getRawComposableName(i), getDataComposableName(i)]),
     })
 