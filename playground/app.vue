49e34fdf3150f23fe9affe5dee9123702b340a0c
Johann Schopplich <mail@johannschopplich.com>
2024-05-29 10:47:17 +0200

    refactor: playground meta tags

--- playground/app.vue
+++ playground/app.vue
@@ -1,13 +1,10 @@
 <script setup lang="ts">
-import { useHead } from '#imports'
+import { useServerHead } from '#imports'
 
-useHead({
+useServerHead({
   title: 'nuxt-api-party',
   link: [
-    {
-      rel: 'stylesheet',
-      href: 'https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css',
-    },
+    { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css' },
   ],
 })
 </script>
