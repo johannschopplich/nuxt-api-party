3d95a88fedb0692ebe10f8034c38b1d5c63d3ce2
Johann Schopplich <mail@johannschopplich.com>
2024-06-20 21:14:20 +0200

    feat: upgrade `openapi-typescript-helpers`

--- src/runtime/openapi.ts
+++ src/runtime/openapi.ts
@@ -3,23 +3,14 @@ import type { MaybeRefOrGetter } from 'vue'
 import type { NuxtError } from 'nuxt/app'
 import type {
   ErrorResponse,
-  FilterKeys,
   MediaType,
   OperationRequestBodyContent,
   ResponseObjectMap,
   SuccessResponse,
 } from 'openapi-typescript-helpers'
 
-export type FetchResponseData<T> = FilterKeys<
-  SuccessResponse<ResponseObjectMap<T>>,
-  MediaType
->
-export type FetchResponseError<T> = NuxtError<
-  FilterKeys<
-    ErrorResponse<ResponseObjectMap<T>>,
-    MediaType
-  >
->
+export type FetchResponseData<T> = SuccessResponse<ResponseObjectMap<T>, MediaType>
+export type FetchResponseError<T> = NuxtError<ErrorResponse<ResponseObjectMap<T>, MediaType>>
 
 export type MethodOption<M, P> = 'get' extends keyof P ? { method?: M } : { method: M }
 
