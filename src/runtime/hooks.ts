de36968fa0b5111a474cacae597fc05f4611b817
Matthew Messinger <mattmess1221@gmail.com>
2024-11-11 17:07:17 -0500

    fix: tests

--- src/runtime/hooks.ts
+++ src/runtime/hooks.ts
@@ -1,20 +1,30 @@
 import type { FetchHooks } from 'ofetch'
 
 type Arrayify<T> = { [P in keyof T]-?: Extract<T[P], unknown[]> }
-type Hooks = Arrayify<Pick<FetchHooks, 'onRequest' | 'onResponse'>>
 
-export function mergeFetchHooks(...hooks: FetchHooks[]): Hooks {
-  const result: Hooks = {
+function maybePush<T>(array: T[], values: T | T[] | undefined): void {
+  if (values) {
+    if (Array.isArray(values)) {
+      array.push(...values)
+    }
+    else {
+      array.push(values)
+    }
+  }
+}
+
+export function mergeFetchHooks(...hooks: FetchHooks[]): FetchHooks {
+  const result = {
     onRequest: [],
     onResponse: [],
-  }
+    onRequestError: [],
+    onResponseError: [],
+  } as Arrayify<FetchHooks>
 
   hooks.forEach((hook) => {
-    for (const name of Object.keys(result) as (keyof Hooks)[]) {
-      if (hook) {
-        result[name].push(...(Array.isArray(hook) ? hook : [hook]))
-      }
-    }
+    Object.entries(hook).forEach(([key, value]) => {
+      maybePush(result[key as keyof typeof result], value)
+    })
   })
 
   return result
