14dfbb235ae61504e98e15a90ab2c87d887caeed
Johann Schopplich <mail@johannschopplich.com>
2025-01-13 21:52:55 +0100

    build: exclude `dist` from typechecl

--- test/fixture/pages/$testApi.vue
+++ test/fixture/pages/$testApi.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { $testApi, useTestResult } from '#imports'
 import type { TestApiTodo } from '~/types'
+import { $testApi, useTestResult } from '#imports'
 
 const json = await $testApi<TestApiTodo>('todos')
 const blob = await $testApi<Blob>('blob')
