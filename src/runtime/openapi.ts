95a6b7edf61b60df419597a15ee51e915a6cc5e0
Johann Schopplich <mail@johannschopplich.com>
2025-05-29 18:48:08 +0200

    fix: update headers type to `HeadersInit`

--- src/runtime/openapi.ts
+++ src/runtime/openapi.ts
@@ -17,7 +17,7 @@ export type MethodOption<M, P> = 'get' extends keyof P ? { method?: M } : { meth
 
 export type ParamsOption<T> = T extends { parameters?: any, query?: any }
   ? Omit<T['parameters'], 'cookie' | 'header'> & {
-    headers?: T['parameters']['header'] | Record<string, unknown>
+    headers?: T['parameters']['header'] | HeadersInit
   }
   : Record<string, unknown>
 
