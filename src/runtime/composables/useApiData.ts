336b77e0beff8e5f3d8a2c14affd1f94928fdc9d
Matthew Messinger <Matthew.Messinger2@netapp.com>
2025-07-17 10:29:43 -0400

    feat: register data composables for autoKey

--- src/runtime/composables/useApiData.ts
+++ src/runtime/composables/useApiData.ts
@@ -77,6 +77,7 @@ export type UseApiDataOptions<T> = Pick<
 export type UseApiData = <T = unknown>(
   path: MaybeRefOrGetter<string>,
   opts?: UseApiDataOptions<T>,
+  autoKey?: string
 ) => AsyncData<T | null, NuxtError>
 
 export type UseOpenAPIDataOptions<
@@ -111,8 +112,10 @@ export type UseOpenAPIData<Paths> = <
 export function _useApiData<T = unknown>(
   endpointId: string,
   path: MaybeRefOrGetter<string>,
-  opts: UseApiDataOptions<T> = {},
+  arg1?: UseApiDataOptions<T> | string,
+  arg2?: string,
 ) {
+  const [opts = {}, autoKey] = typeof arg1 === 'string' ? [{}, arg1] : [arg1, arg2]
   const apiParty = useRuntimeConfig().public.apiParty as Required<ModuleOptions>
   const {
     server,
@@ -136,6 +139,7 @@ export function _useApiData<T = unknown>(
   const _path = computed(() => resolvePathParams(toValue(path), toValue(pathParams)))
   const _key = computed(key === undefined
     ? () => CACHE_KEY_PREFIX + hash([
+        autoKey,
         endpointId,
         _path.value,
         toValue(query),
