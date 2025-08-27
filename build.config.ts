5e2b63530f7b860457e529ad675fdfa5c074621c
Johann Schopplich <mail@johannschopplich.com>
2025-08-27 12:48:22 +0200

    chore: add h3 dependency

--- build.config.ts
+++ build.config.ts
@@ -3,10 +3,5 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
   externals: [
     'openapi-typescript',
-    // TODO: Why does Nuxt module builder not exclude these?
-    'h3',
-    'crossws',
-    'cookie-es',
-    'iron-webcrypto',
   ],
 })
