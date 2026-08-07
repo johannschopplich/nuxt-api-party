3eba318234077e58a2f9dcb41866c9ab9c6c2987
Johann Schopplich <johann@schopplich.com>
2026-08-07 19:42:58 +0200

    chore: drop the unused twitter export

--- docs/.vitepress/meta.ts
+++ docs/.vitepress/meta.ts
@@ -4,4 +4,3 @@ export const ogImage = `${ogUrl}og.jpg`
 
 export const github = 'https://github.com/johannschopplich/nuxt-api-party'
 export const releases = 'https://github.com/johannschopplich/nuxt-api-party/releases'
-export const twitter = 'https://twitter.com/jschopplich'
